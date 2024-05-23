c51f6c49758690b26e2dc9d56024ecd597d4b471
danielmichaneck <daniel.michaneck@gmail.com>
2024-05-23 10:21:28 +0200

    Finished PriceForGroup().

--- Program.cs
+++ Program.cs
@@ -14,7 +14,11 @@
         private static bool MainMenu()
         {
             Console.WriteLine("Välkommen till huvudmenyn.\n" +
-                "Vänligen skriv in en siffra för att välja och testa en funktion.\n");
+                "Vänligen skriv in en siffra för att välja och testa en funktion.\n" +
+                "0) Avsluta programmet.\n" +
+                "1) Räkna ut pris för en biobesökare.\n" +
+                "2) Räkna ut pris för en grupp biobesökare.\n" +
+                "3) Upprepa en inmatning 10 gånger.\n");
             string input = "";
             input = Console.ReadLine()!;
             Console.WriteLine();
@@ -59,9 +63,10 @@
             public const string option3 = "3";
         }
 
-        private static void PriceByAge()
+        private static int PriceByAge()
         {
             bool loop = true;
+            int price = 0;
             Console.WriteLine("Vänligen skriv in biobesökarens ålder.\n");
             do
             {
@@ -75,36 +80,71 @@
                         if (age < 20)
                         {
                             Console.WriteLine("Ungdomspris: 80kr.");
+                            price = 80;
                         }
                         else if (age > 64)
                         {
                             Console.WriteLine("Pensionärspris: 90kr.");
+                            price = 90;
                         }
                         else
                         {
                             Console.WriteLine("Standardpris: 120kr.");
+                            price = 120;
                         }
                     }
                     Console.WriteLine();    
                 }
                 else PrintFaultyInput();
             } while (loop);
+            return price;
         }
 
         private static void PriceForGroup()
         {
+            int groupSize = 0;
             Console.WriteLine("Vänligen skriv in antal biobesökare.\n");
             bool loop = true;
             do
             {
-                if (int.TryParse(Console.ReadLine(), out var groupSize))
+                if (int.TryParse(Console.ReadLine(), out int size))
                 {
-                    if (groupSize < 0)
+                    if (size < 0)
                         Console.WriteLine("Antalet kan inte vara mindre än noll.");
-                    else loop = false;
+                    else
+                    {
+                        loop = false;
+                        groupSize = size;
+                    }
                     Console.WriteLine();
                 }
                 else PrintFaultyInput();
+            } while (loop);
+            int totalPrice = 0;
+            for (int i = 0; i < groupSize; i++)
+            {
+                totalPrice += PriceByAge();
+            }
+            Console.WriteLine($"Den totala kostnaden för de {groupSize} biobesökarna är {totalPrice}kr.\n");
+        }
+
+        private static void RepeatInput()
+        {
+            string input = "";
+            input = Console.ReadLine()!;
+            Console.WriteLine();
+            bool loop = true;
+            do
+            {
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    PrintFaultyInput();
+                }
+                else
+                {
+
+                }
+                    
             } while (loop);
         }
     }
