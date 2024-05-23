b039a126ade894050bd99a6f8d2ca9242b9b1461
danielmichaneck <daniel.michaneck@gmail.com>
2024-05-23 09:33:03 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Flow_Control
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
